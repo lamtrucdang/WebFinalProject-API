c82bf02df43c8fc7e9ffb58190e9521de7f6f7f3
Lam Truc Dang <19521736@gm.uit.edu.vn>
2022-10-20 23:47:56 +0700

    init signup api

---
+++ api/routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+router.post('/signup', (req, res, next) => {')
+
+module.exports = router;
