28d23321857e96dc6fdf09e42ded1fb665a15c5a
Le Ngoc Thuy An <19521176@gm.uit.edu.vn>
2022-12-22 00:26:43 +0700

    new api

--- api/index.js
+++ api/index.js
@@ -5,11 +5,11 @@ const mogoose = require("mongoose");
 const port = process.env.PORT || 3001
 const { default: mongoose } = require('mongoose');
 const authRoute= require("./routes/auth");
-// const userRoute = require("./routes/users");
-// const postRoute = require("./routes/posts");
-// const categoryRoute = require("./routes/categories");
-// const multer = require("multer");
-// const path = require("path");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
 
 dotenv.config();
@@ -19,24 +19,24 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, () => {
   console.log("Connected to MongoDB");
 });
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-// const upload = multer({ storage: storage });
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   res.status(200).json("File has been uploaded");
-// });
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/posts", postRoute);
-// app.use("/api/categories", categoryRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
   app.listen(port, () => {
     console.log('Connected!');
